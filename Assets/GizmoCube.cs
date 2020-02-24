186bdb954b8d577400828e83e31ebded37c7995c
korteh <superkorteh@yandex.ru>
2020-02-24 17:43:40 +0300

    Level Geometry
    построил твердую часть уровня

---
+++ Assets/GizmoCube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GizmoCube : MonoBehaviour
+{
+    [Header("Rect Seetings")]
+    public float width;
+    public float height;
+    [Header("Label Seetings")]
+    public string Text = "Put text here";
+    public Vector3 TextPosition;
+    void OnDrawGizmos()
+    {
+        
+        Gizmos.color = Color.yellow;
+        Handles.Label(transform.position+TextPosition, Text);
+        Gizmos.DrawWireCube(transform.position,new Vector3(width,height,1));
+    }
+}
