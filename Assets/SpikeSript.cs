d3cff3b9e0ed5422ed59f1e12603e5b4abda2339
korteh <superkorteh@yandex.ru>
2020-02-27 00:43:02 +0300

    Spikes, wind, presses

---
+++ Assets/SpikeSript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeSript : MonoBehaviour
+{
+    [SerializeField] private float attackPower = 500f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var playerMovement = collision.gameObject.GetComponent<Movement>();
+
+        if (playerMovement != null)
+        {
+            playerMovement.SetState(new Stunned(playerMovement));
+            var attackImpulseDirection = (playerMovement.transform.position - transform.position).normalized;
+            playerMovement.Rigidbody2d.AddForce(attackImpulseDirection * attackPower);
+            //transform.parent.gameObject.SetActive(false);
+        }
+
+        var AddScore = collision.gameObject.GetComponent<ScoreCounter>();
+        if (AddScore != null)
+        {
+            AddScore.ChangeScore(-10);
+        }
+
+    }
+}
