3d706dc24934ae45be107075685f5ba77692125e
gerh667 <gerh667@gmail.com>
2020-02-28 22:13:32 +0300

    hotfix for stun

--- Assets/Scripts/PlayerStateMachine/States/Stunned.cs
+++ Assets/Scripts/PlayerStateMachine/States/Stunned.cs
@@ -4,7 +4,10 @@ public class Stunned : State
 {
     private float stunTime;
 
-    public Stunned(Movement movement, float stunTime = 0.3f) : base(movement) { }
+    public Stunned(Movement movement, float stunTime = 0.3f) : base(movement) 
+    {
+        this.stunTime = stunTime;
+    }
 
     public override void OnStart()
     {
