04b42bdaff9cfbbd960bab9e56624c44fa059f40
gerh667 <gerh667@gmail.com>
2020-04-28 11:55:09 +0300

    end level setup (in progress)

---
+++ Assets/Scripts/EndLevelTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevelTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.GetComponent<Movement>())
+        {
+            Debug.Log("You reached end of a level!");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/EndLevelTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndLevelTimer : MonoBehaviour
+{
+    [SerializeField] private Text timerText;
+    
+    private float currentTime;
+    private bool isActive;
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            currentTime -= Time.deltaTime;
+            int currentTimeInt = (int)currentTime;
+            timerText.text = $"{currentTimeInt / 60}:{currentTimeInt % 60}";
+
+            if (currentTime <= 0)
+            {
+                Debug.Log("End timer complite. Level ends."); // TODO: connect end level elements
+            }
+        }
+    }
+
+    public void SetTime(float time)
+    {
+        currentTime = time;
+    }
+
+    public void Activate()
+    {
+        isActive = true;
+    }
+}
