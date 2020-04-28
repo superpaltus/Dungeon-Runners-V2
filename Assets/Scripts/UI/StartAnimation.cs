5772ec12f2c68b9005b40540947f18a9d454adbf
gerh667 <gerh667@gmail.com>
2020-04-28 10:43:12 +0300

    start animation added

---
+++ Assets/Scripts/UI/StartAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimation : MonoBehaviour
+{
+    public void OnAnimationStart()
+    {
+        Time.timeScale = 0f;
+        Debug.Log("Time now is: " + Time.timeScale);
+    }
+
+    public void OnAnimationEnd()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Time now is: " + Time.timeScale);
+    }
+}
