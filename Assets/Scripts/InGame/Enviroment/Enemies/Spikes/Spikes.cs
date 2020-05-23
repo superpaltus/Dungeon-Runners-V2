b9f7fe932aa45af0eafe54f741ace9249f053f49
gerh667 <gerh667@gmail.com>
2020-05-23 19:18:09 +0300

    add limit for spawning coins after hit

---
+++ Assets/Scripts/InGame/Enviroment/Enemies/Spikes/Spikes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] private float attackPower = 10f;
+    [SerializeField] private int damage = 10;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var playerMovement = collision.gameObject.GetComponent<Movement>();
+
+        if (playerMovement != null)
+        {
+            var attackImpulseDirection = (playerMovement.transform.position - transform.parent.position).normalized;
+            playerMovement.TakeDamage(attackImpulseDirection, attackPower, damage);
+        }
+    }
+}
--- Assets/Scripts/InGame/PlayerStateMachine/PlyerGoldCollector.cs
+++ Assets/Scripts/InGame/PlayerStateMachine/PlyerGoldCollector.cs
@@ -15,12 +15,16 @@ public class PlyerGoldCollector : MonoBehaviour, IDamageable
 
     public void ChangeGold(int value)
     {
+        int goldBefore = currentGold;
         currentGold = Mathf.Clamp(currentGold += value, 0, int.MaxValue);
         GoldChanged?.Invoke(currentGold);
         Debug.Log($"Current gold is: {currentGold}");
+
         if (value < 0)
         {
-            for (int i = 0; i < 10; i++)
+            int spawningGoldCoinsNumber = goldBefore - currentGold;
+
+            for (int i = 0; i < spawningGoldCoinsNumber; i++)
             {
                 var spawnPosition = transform.position + Vector3.up * offset;
                 var newGold = Instantiate(goldCoinPrefab, spawnPosition, Quaternion.identity);
