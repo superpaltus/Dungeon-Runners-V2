8c189aad85a0a97a99e2fe2eae8a1f43d862f3bb
gerh667 <gerh667@gmail.com>
2020-05-25 22:06:00 +0300

    fix immediatly stop

--- Assets/Scripts/InGame/PlayerStateMachine/States/Idle.cs
+++ Assets/Scripts/InGame/PlayerStateMachine/States/Idle.cs
@@ -2,6 +2,8 @@
 
 public class Idle : State
 {
+    private float stopCoefficient = 1.4f;
+
     public Idle(Movement movement) : base(movement) { }
 
     public override void OnStart()
@@ -13,7 +15,7 @@ public class Idle : State
 
     public override void OnUpdate()
     {
-        movement.Rigidbody2d.velocity = new Vector2(0f, movement.Rigidbody2d.velocity.y);
+        movement.Rigidbody2d.velocity = new Vector2(movement.Rigidbody2d.velocity.x / stopCoefficient, movement.Rigidbody2d.velocity.y);
     }
 
     public override void OnLeftButton()
