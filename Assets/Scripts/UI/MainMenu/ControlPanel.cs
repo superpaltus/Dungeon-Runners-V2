888d72ebfec5c1f27d8d19b9e4829f921b83e3a7
gerh667 <gerh667@gmail.com>
2020-05-26 02:15:18 +0300

    main menu upgrade with controls

---
+++ Assets/Scripts/UI/MainMenu/ControlPanel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPanel : MonoBehaviour
+{
+    [SerializeField] private Button closeButton;
+
+    private void OnEnable()
+    {
+        closeButton.onClick.AddListener(Close);
+    }
+
+    private void OnDisable()
+    {
+        closeButton.onClick.RemoveAllListeners();
+    }
+
+    private void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button buttonStart;
+    [SerializeField] private Button buttonControls;
     [SerializeField] private Button buttonQuit;
     [SerializeField] private int gameSceneIndex;
+    [SerializeField] private GameObject controlsMenu;
 
     private void OnEnable()
     {
         buttonStart.onClick.AddListener(OnButtonStartClick);
+        buttonControls.onClick.AddListener(OnButtonControlsClick);
         buttonQuit.onClick.AddListener(OnButtonQuitClick);
+    }
+
+    private void OnButtonControlsClick()
+    {
+        controlsMenu.SetActive(true);
     }
 
     private void OnButtonStartClick()
